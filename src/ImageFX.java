fa6142e2dddd81534797e889dfad5a085ca13b48
rohan-k02 <rohan.k02@outlook.com>
2023-11-01 11:25:10 +0530

    Created main project file

---
+++ src/ImageFX.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.effect.ColorAdjust;
+import javafx.scene.effect.SepiaTone;
+import javafx.scene.effect.GaussianBlur;
+import javafx.stage.Stage;
+
+public class ImageFX extends Application
+{
+    private Image originalImage;
+    private ImageView imageView;   
+    
+    public void start(Stage primaryStage)
+    {
+    
+    }
+}
